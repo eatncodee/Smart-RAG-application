f85ece904808ff2dfd7b689db0eda43205107e62
Rudraksh Gulati <rudrakshgulati5@gmail.com>
2026-02-18 15:14:24 +0530

    removed mock functions

--- app/routes/websocket.py
+++ app/routes/websocket.py
@@ -5,25 +5,6 @@ from app.services.streaming import stream_rag_response
 
 router=APIRouter()
 
-
-@router.websocket("/ws/echo")
-async def websocket_echo(websocket:WebSocket):
-
-    await websocket.accept()
-    print("Client Connected")
-
-    try:
-        while True:
-            message=await websocket.receive_text()
-            print(f"📤 Recieved: {message}")
-
-            response=f"Echo: {message}"
-            await websocket.send_text(response)
-            print(f"📤 Sent: {response}")
-    
-    except WebSocketDisconnect:
-        print("❌ Client disconnected")
-
 @router.websocket("/ws/chat")
 async def websocket_chat(websocket:WebSocket):
     await websocket.accept()
