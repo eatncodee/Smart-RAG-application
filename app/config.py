0069671539565add818cc946e40ea6618ba70739
Rudraksh Gulati <rudrakshgulati5@gmail.com>
2026-03-08 17:44:31 +0530

    added video and gif demo

--- app/config.py
+++ app/config.py
@@ -6,7 +6,7 @@ load_dotenv()
 class Settings:
     GOOGLE_API_KEY = os.getenv("key")
     EMBEDDING_MODEL = "models/gemini-embedding-001"
-    CHAT_MODEL = "gemini-2.5-flash"
+    CHAT_MODEL = "gemini-2.5-flash-lite"
     CHROMA_DB_PATH = "./chroma_db"
     COLLECTION_NAME = "docs"
 
